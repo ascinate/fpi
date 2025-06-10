aea9719880a297e2b8b8d5a30c8a5c75a52a380a
Pritam-Ghosh <pritam7599@gmail.com>
2025-06-10 17:30:29 +0530

    fpi update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,37 +1,50 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Navbar from '../components/Navbar'
-
-
-//Image
-import fone from "/f1.png"
-import ftwo from "/f2.png"
-import fthree from "/f3.png"
-import ffour from "/f4.png"
-import ffive from "/f5.png"
+import React from 'react';
+import Navbar from '../components/Navbar';
+import ione from "/fone.svg";
+import ithree from "/f2.svg";
+import ifour from "/f3.svg";
+import isix from "/f4.svg";
+import five from "/f5.svg";
+import CustomCursor from '../components/CustomCursor';
 
 
 function Home() {
     return (
         <>
+            <CustomCursor />
             <Navbar />
-            <main>
-                <section className='section-hero'>
-                    <div className='wrapper'>
-                        <div className='container-hero flex items-center gap-52 '>
-                            <div>
+
+            <main className='float-left w-full'>
+                <section className='section-hero float-left w-full'>
+
+                    <div className='container mx-auto py-4'>
+                        <div className='container-hero relative'>
+                            <div className='banner-tms01'>
                                 <h1 className='hero-main-text'>Your go-to URL shortener and link-in-bio platform</h1>
-                                <p className='sub-text mb-11'>Seamlessly manage and track all your links.Elevate your brand today!</p>
-                                <button className='theme-color-btn' id=''>Get Started</button>
-                                <button className='white-color-btn ' id=''>Try Demo</button>
+                                <p className='sub-text mb-11'>Seamlessly manage and track all your links. Elevate your brand today!</p>
+                                <button className='theme-color-btn brn-btn' id=''>Get Started</button>
+                                <button className='white-color-btn brn-btn' id=''>Try Demo</button>
                             </div>
-                            <div className='hero-img'>
-                                <img src={fone} alt="" className='fone' />
-                                <img src={ftwo} alt="" className='ftwo' />
-                                <img src={ffour} alt="" className='ffour' />
-                                <img src={fthree} alt="" className='fthree' />
-                                <img src={ffive} alt="" className='ffive' width={300} height={100}/>
+
+                            <div className='ithree'>
+                                <img src={ithree} alt="b" />
                             </div>
+                            <div className='ifour'>
+                                <img src={ifour} alt="b" />
+                            </div>
+                            <div className='ione-face'>
+                                <img src={ione} alt="g" />
+                            </div>
+                            <div className='isix'>
+                                <img src={isix} alt="h" />
+                            </div>
+                                <div className='five'>
+
+                            <img src={five} alt="" />
+                                </div>
+
+
+
                         </div>
                     </div>
                 </section>
@@ -42,4 +55,4 @@ function Home() {
     )
 }
 
-export default Home
+export default Home;
